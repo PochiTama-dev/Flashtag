8591d7fc9f47fc95d67f93328764a572e4a0d200
NachoMaraude <maraudenacho@gmail.com>
2025-03-10 12:23:30 -0300

    FT-02 // Iconos y estilos de tabla

--- frontend/src/layout/FlashTagProducts/components/ProductsTable/ProductsTable.tsx
+++ frontend/src/layout/FlashTagProducts/components/ProductsTable/ProductsTable.tsx
@@ -2,6 +2,11 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./ProductsTable.scss";
 import GenericTable from "../../../../components/GenericTable/GenericTable";
+import { FaWifi } from "react-icons/fa";
+import { FaLink } from "react-icons/fa6";
+import { MdNfc } from "react-icons/md";
+import { IoFilter } from "react-icons/io5";
+import { IoMdArrowDown, IoMdArrowUp } from "react-icons/io";
 
 const ProductsTable: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -42,27 +47,60 @@ const ProductsTable: React.FC = () => {
     item["Título producto"].toLowerCase().includes(searchTerm.toLowerCase())
   );
 
+  const getIcon = (tipo: string) => {
+    switch (tipo) {
+      case "Wifi":
+        return <FaWifi className="icon-wifi" />;
+      case "Link":
+        return <FaLink className="icon-link" />;
+      case "NFC":
+        return <MdNfc className="icon-nfc" />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="products-table">
-      <h2>
-        <svg
-          width="34"
-          height="34"
-          viewBox="0 0 34 34"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <rect width="34" height="34" rx="17" fill="#EEF3FE" />
-          <path
-            d="M9.5 15.3333V10.3333C9.5 10.0972 9.58 9.89944 9.74 9.74C9.9 9.58056 10.0978 9.50056 10.3333 9.5H15.3333C15.5694 9.5 15.7675 9.58 15.9275 9.74C16.0875 9.9 16.1672 10.0978 16.1667 10.3333V15.3333C16.1667 15.5694 16.0867 15.7675 15.9267 15.9275C15.7667 16.0875 15.5689 16.1672 15.3333 16.1667H10.3333C10.0972 16.1667 9.89944 16.0867 9.74 15.9267C9.58056 15.7667 9.50056 15.5689 9.5 15.3333ZM11.1667 14.5H14.5V11.1667H11.1667V14.5ZM9.5 23.6667V18.6667C9.5 18.4306 9.58 18.2328 9.74 18.0733C9.9 17.9139 10.0978 17.8339 10.3333 17.8333H15.3333C15.5694 17.8333 15.7675 17.9133 15.9275 18.0733C16.0875 18.2333 16.1672 18.4311 16.1667 18.6667V23.6667C16.1667 23.9028 16.0867 24.1008 15.9267 24.2608C15.7667 24.4208 15.5689 24.5006 15.3333 24.5H10.3333C10.0972 24.5 9.89944 24.42 9.74 24.26C9.58056 24.1 9.50056 23.9022 9.5 23.6667ZM11.1667 22.8333H14.5V19.5H11.1667V22.8333ZM17.8333 15.3333V10.3333C17.8333 10.0972 17.9133 9.89944 18.0733 9.74C18.2333 9.58056 18.4311 9.50056 18.6667 9.5H23.6667C23.9028 9.5 24.1008 9.58 24.2608 9.74C24.4208 9.9 24.5006 10.0978 24.5 10.3333V15.3333C24.5 15.5694 24.42 15.7675 24.26 15.9275C24.1 16.0875 23.9022 16.1672 23.6667 16.1667H18.6667C18.4306 16.1667 18.2328 16.0867 18.0733 15.9267C17.9139 15.7667 17.8339 15.5689 17.8333 15.3333ZM19.5 14.5H22.8333V11.1667H19.5V14.5ZM22.8333 24.5V22.8333H24.5V24.5H22.8333ZM17.8333 19.5V17.8333H19.5V19.5H17.8333ZM19.5 21.1667V19.5H21.1667V21.1667H19.5ZM17.8333 22.8333V21.1667H19.5V22.8333H17.8333ZM19.5 24.5V22.8333H21.1667V24.5H19.5ZM21.1667 22.8333V21.1667H22.8333V22.8333H21.1667ZM21.1667 19.5V17.8333H22.8333V19.5H21.1667ZM22.8333 21.1667V19.5H24.5V21.1667H22.8333Z"
-            fill="#613FC2"
-          />
-        </svg>
-        Productos Flashtag
-      </h2>
-      <label>Administra y gestiona tus precomprados.</label>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <div>
+          <h2>
+            <svg
+              width="34"
+              height="34"
+              viewBox="0 0 34 34"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect width="34" height="34" rx="17" fill="#EEF3FE" />
+              <path
+                d="M9.5 15.3333V10.3333C9.5 10.0972 9.58 9.89944 9.74 9.74C9.9 9.58056 10.0978 9.50056 10.3333 9.5H15.3333C15.5694 9.5 15.7675 9.58 15.9275 9.74C16.0875 9.9 16.1672 10.0978 16.1667 10.3333V15.3333C16.1667 15.5694 16.0867 15.7675 15.9267 15.9275C15.7667 16.0875 15.5689 16.1672 15.3333 16.1667H10.3333C10.0972 16.1667 9.89944 16.0867 9.74 15.9267C9.58056 15.7667 9.50056 15.5689 9.5 15.3333ZM11.1667 14.5H14.5V11.1667H11.1667V14.5ZM9.5 23.6667V18.6667C9.5 18.4306 9.58 18.2328 9.74 18.0733C9.9 17.9139 10.0978 17.8339 10.3333 17.8333H15.3333C15.5694 17.8333 15.7675 17.9133 15.9275 18.0733C16.0875 18.2333 16.1672 18.4311 16.1667 18.6667V23.6667C16.1667 23.9028 16.0867 24.1008 15.9267 24.2608C15.7667 24.4208 15.5689 24.5006 15.3333 24.5H10.3333C10.0972 24.5 9.89944 24.42 9.74 24.26C9.58056 24.1 9.50056 23.9022 9.5 23.6667ZM11.1667 22.8333H14.5V19.5H11.1667V22.8333ZM17.8333 15.3333V10.3333C17.8333 10.0972 17.9133 9.89944 18.0733 9.74C18.2333 9.58056 18.4311 9.50056 18.6667 9.5H23.6667C23.9028 9.5 24.1008 9.58 24.2608 9.74C24.4208 9.9 24.5006 10.0978 24.5 10.3333V15.3333C24.5 15.5694 24.42 15.7675 24.26 15.9275C24.1 16.0875 23.9022 16.1672 23.6667 16.1667H18.6667C18.4306 16.1667 18.2328 16.0867 18.0733 15.9267C17.9139 15.7667 17.8339 15.5689 17.8333 15.3333ZM19.5 14.5H22.8333V11.1667H19.5V14.5ZM22.8333 24.5V22.8333H24.5V24.5H22.8333ZM17.8333 19.5V17.8333H19.5V19.5H17.8333ZM19.5 21.1667V19.5H21.1667V21.1667H19.5ZM17.8333 22.8333V21.1667H19.5V22.8333H17.8333ZM19.5 24.5V22.8333H21.1667V24.5H19.5ZM21.1667 22.8333V21.1667H22.8333V22.8333H21.1667ZM21.1667 19.5V17.8333H22.8333V19.5H21.1667ZM22.8333 21.1667V19.5H24.5V21.1667H22.8333Z"
+                fill="#613FC2"
+              />
+            </svg>
+            Productos Flashtag
+          </h2>
+          <label>Administra y gestiona tus precomprados.</label>
+        </div>
+        <div style={{ display: "flex", flexDirection: "row", gap: "10px" }}>
+          <button className="products-buttons-top">
+            {" "}
+            <IoMdArrowDown className="icon-export" />
+            Importar
+          </button>
+          <button className="products-buttons-top">
+            <IoMdArrowUp className="icon-export" /> Exportar
+          </button>
+        </div>
+      </div>
       <div className="table-controls">
-        <h4>QR creados</h4>
+        <h3>QR creados</h3>
         <div className="search-container">
           <input
             type="text"
@@ -91,9 +129,11 @@ const ProductsTable: React.FC = () => {
           style={{
             backgroundColor: "white",
             color: "black",
-            border: "1px solid gray",
+            border: "1px solid #ddd",
           }}
+          className="products-table-buttons"
         >
+          <IoFilter className="icon-filter" />
           Filtros
         </button>
         <button
@@ -103,8 +143,8 @@ const ProductsTable: React.FC = () => {
             gap: "10px",
             alignItems: "center",
           }}
+          className="products-table-buttons"
         >
-          {" "}
           <span
             onClick={() => navigate("/qrDesign")}
             style={{ fontSize: "26px" }}
@@ -113,12 +153,15 @@ const ProductsTable: React.FC = () => {
           </span>{" "}
           Asignar
         </button>
-        <button>Exportar</button>
+        <button className="products-table-buttons">
+          {" "}
+          <IoMdArrowUp className="icon-export" /> Exportar
+        </button>
         <button
           style={{
             backgroundColor: "white",
             color: "black",
-            border: "1px solid gray",
+            border: "1px solid #ddd",
             padding: "8px",
           }}
         >
@@ -143,7 +186,11 @@ const ProductsTable: React.FC = () => {
         columnTitles={columnTitles}
         data={filteredData.map((item) => ({
           ...item,
-          Tipo: <span className="tipo">{item.Tipo}</span>,
+          Tipo: (
+            <span className="tipo">
+              {getIcon(item.Tipo)} {item.Tipo}
+            </span>
+          ),
           Link: <span className="link">{item.Link}</span>,
           Estado: (
             <span className={`estado-${item.Estado.toLowerCase()}`}>
@@ -151,7 +198,6 @@ const ProductsTable: React.FC = () => {
             </span>
           ),
         }))}
-        headerTitle="QR Creados"
       />
     </div>
   );
